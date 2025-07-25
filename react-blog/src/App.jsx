e23bbd94493bbd10eb7efb25a7ae6644cc4747f3
Vedant Kadam <vedantkd23@gmail.com>
2025-07-25 19:52:06 +0530

    Exporting importing without using default

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -1,13 +1,36 @@
 import Header from "./Header"
-
+import {Login} from "./UserComponent"
 function App() {
   return(
     <>
     <h1>Hello</h1>
     <h1>Code step by step</h1>
     <Header></Header>
+    <Fruit></Fruit>
+    <Colour></Colour>
+    <Login></Login>
+    {sum(30,20)}
     </>
     
   )
+}
+
+
+function Fruit() {
+  return(
+    <>
+    <h1>Apple</h1></>
+  )
+}
+
+function Colour() {
+  return(
+    <>
+    <h1>Red</h1></>
+  )
+}
+
+function sum(i,j) {
+  return i+j;
 }
 export default App
