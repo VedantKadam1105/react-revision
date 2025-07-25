5d75dd7570e1b41c96e842276975736d1d6a4130
Vedant Kadam <vedantkd23@gmail.com>
2025-07-25 19:36:27 +0530

    Initial Commit

---
+++ react-blog/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+
+function App() {
+  return(
+    <>
+    <h1>Hello</h1>
+    <h1>Code step by step</h1>
+    <Header></Header>
+    </>
+    
+  )
+}
+export default App
---
+++ react-blog/src/Header.jsx
@@ -0,0 +1,6 @@
+function Header() {
+    return <div>
+        <h4>header file</h4>
+    </div>
+}
+export default Header
